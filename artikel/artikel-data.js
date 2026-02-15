90eb6690b2f1c74528c8df26a75dec787d93f985
rostergallery <muhamadadilatu@gmail.com>
2026-02-16 00:05:26 +0700

    Update artikel-data.js

--- artikel/artikel-data.js
+++ artikel/artikel-data.js
@@ -1,15 +1,5 @@
 const defaultArticles = [
   {
-    id: "contoh-artikel-pertama",
-    title: "Kelebihan GRC untuk Ornamen Bangunan",
-    author: "Admin",
-    date: "2026-02-15",
-    category: "Edukasi Material",
-    media: [
-      {
-        url: "../assets/images/profil-jendela-1.jpeg",
-        type: "image/jpeg"
-      }
     ],
     content: "<p>Ini adalah artikel pertama percobaan.</p>"
   }
