a63105d9351d4c3415575d0f519b8464f12a2701
rostergallery <muhamadadilatu@gmail.com>
2026-02-17 00:18:30 +0700

    Delete artikel/js directory

--- artikel/js/supabase.js
+++
@@ -1,9 +0,0 @@
-// Pastikan CDN supabase sudah dipanggil sebelum file ini
-// https://cdn.jsdelivr.net/npm/@supabase/supabase-js
-
-const SUPABASE_URL = "https://tcuimqhiilfervvjfcey.supabase.co";
-const SUPABASE_KEY = "sb_publishable_LINoyHDPvUvWDVB6OHBXWg_1BK_EAIu";
-
-// Global variable
-window.supabase = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
-
