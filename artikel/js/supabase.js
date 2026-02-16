b93e591df632f4ed381062006880904122cedb4d
rostergallery <muhamadadilatu@gmail.com>
2026-02-16 18:25:46 +0700

    Update supabase.js

--- artikel/js/supabase.js
+++ artikel/js/supabase.js
@@ -1,1 +1,9 @@
+import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm'
 
+const supabaseUrl = 'https://tcuimqhiilfervvjfcey.supabase.co'
+const supabaseKey = 'sb_publishable_LINoyHDPvUvWDVB6OHBXWg_1BK_EAIu'
+
+export const supabase = createClient(
+  supabaseUrl,
+  supabaseKey
+)
